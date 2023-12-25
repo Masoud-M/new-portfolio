b3cf0aff19be5ed1ddae34fba27ce9d822ed9d78
Masoud-M <masoudmoghaddam999@gmail.com>
2023-12-25 13:25:48 +0330

    fixed the project pictures so they would roll up on hover and added other projects

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,43 +1,57 @@
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import { IoOpenOutline } from "react-icons/io5";
+import { BsGithub } from "react-icons/bs";
 
 function ProjectItem({ image, demo, title, description, techStack, srcLink }) {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
   return (
-    <div className="flex flex-col gap-12 p-5 shadow-xl bg-gray-50 md:flex-row rounded-3xl">
-      <div className="box-content h-auto overflow-hidden">
+    <div className="flex flex-col gap-8 lg:gap-20 p-5 shadow-xl bg-gray-50 lg:flex-row rounded-3xl w-fit h-[750px] lg:h-[400px] max-w-[570px] lg:max-w-[990px]">
+      <div className=" mx-auto overflow-hidden max-w-[530px] max-h-[360px] rounded-3xl shadow-md cursor-pointer h-auto">
         <img
-          className="mx-auto w-[500px] h-[400px] object-cover object-top hover:object-bottom image-scroll ease-linear overflow-hidden rounded-3xl cursor-pointer shadow-md"
+          className="transition-all duration-[3000ms] ease-linear translate-y-0 hover:-translate-y-[74%]"
           src={image}
           alt={`${title} image`}
         />
       </div>
 
-      <div className="w-full">
+      <div className="flex flex-col w-[300px] mx-auto justify-center">
         <h3 className="mb-5 text-xl font-bold">{title}</h3>
         <p className="text-gray-600 text-lg/6">{description}</p>
         <div className="flex flex-row items-center justify-center w-full gap-4 mt-4">
           {techStack.map((item) => (
             <div
-              className="px-3 py-1.5 mt-2 bg-white rounded-md shadow-md"
+              className="px-3 py-1.5 mt-2 bg-white rounded-md shadow-md font-semibold text-nowrap"
               key={item}
             >
               {item}
             </div>
           ))}
         </div>
-        <div className="flex flex-row justify-between px-4 mt-8 mb-4">
-          <button className="px-2 py-1 mx-2 font-semibold ">
-            <a href={srcLink} target="_blank" rel="noopener noreferrer">
+        <div className="flex flex-row content-end mt-8 justify-evenly">
+          <button className="px-2 py-1 mx-2 font-semibold transition-all hover:text-blue-500">
+            <a
+              className="flex items-center justify-center gap-2 text-xl text-nowrap"
+              href={srcLink}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Source Code
+              <BsGithub />
             </a>
           </button>
-          <button className="px-2 py-1 mx-2 font-semibold">
-            <a href={demo} target="_blank" rel="noopener noreferrer">
+          <button className="px-2 py-1 mx-2 font-semibold transition-all hover:text-blue-500">
+            <a
+              className="flex items-center justify-center gap-2 text-xl text-nowrap"
+              href={demo}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               Demo
+              <IoOpenOutline />
             </a>
           </button>
         </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,10 +5,29 @@ import SortingVisualizer from "../assets/sorting-visualizer.png";
 function Projects() {
   return (
     <section id="projects" className="bg-gray-100">
-      <div className="w-[90%] lg:w-[65%] mx-auto flex flex-col items-center text-center  py-28 transition-all">
+      <div className="w-[90%] 2xl:w-[65%] mx-auto flex flex-col items-center text-center gap-20  py-28 transition-all">
         <h2 className="mb-10 text-4xl font-semibold text-blue-500 md:text-6xl">
           Projects
         </h2>
+
+        <ProjectItem
+          image={CarRental}
+          title="Car Rental"
+          description="A car rental website is an online platform that allows users to rent cars for personal or business use. The website provides an interface for searching, comparing, and reserving cars."
+          techStack={["Next JS", "TypeScript", "Tailwind"]}
+          srcLink="https://github.com/Masoud-M/car-rental"
+          demo="https://647eead214d0787e130c91b3--delicate-tiramisu-c6cc9e.netlify.app/"
+        />
+
+        <ProjectItem
+          image={GymImg}
+          title="Gym Website"
+          description="A gym website is a comprehensive resource for fitness information, class schedules, membership options, and tools to help users achieve their fitness goals."
+          techStack={["React", "CSS"]}
+          srcLink="https://github.com/Masoud-M/gym-website"
+          demo="https://fitclub-1my.pages.dev/"
+        />
+
         <ProjectItem
           image={SortingVisualizer}
           title="Sorting Visualizer"
